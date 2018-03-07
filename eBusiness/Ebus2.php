422af89887f064b582506e8176e05f4551245cd9
Chloe Cooley <117459314@umail.ucc.ie>
2018-03-07 10:16:39 +0000

    45th Commit - Errors

--- eBusiness/Ebus2.php
+++ eBusiness/Ebus2.php
@@ -16,7 +16,7 @@ session_start();
         
         <!-- jQuery -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-         <script type="text/javascript" src="ebus2_validator.js"></script>
+         <script  src="ebus2_validator.js"></script>
        
     </head>
     <body>
